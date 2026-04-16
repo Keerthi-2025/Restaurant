90b30329e89fa21e0724aeb512d7c36383267e7c
Keerthi <rhkeerthi3@gmail.com>
2026-04-16 12:39:56 +0530

    completed file

--- src/main/java/com/learning/restaurant/services/StorageService.java
+++ src/main/java/com/learning/restaurant/services/StorageService.java
@@ -1,4 +1,13 @@
 package com.learning.restaurant.services;
 
+import com.nimbusds.jose.util.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+
 public interface StorageService {
+
+    String store(MultipartFile file, String filename);  //store multipart file
+    Optional<Resource> loadAsResource(String id); //upload that file as resource
 }
