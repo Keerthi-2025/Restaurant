321c008a45bd2fe199ab5226b277991c072e8e65
Keerthi <rhkeerthi3@gmail.com>
2026-04-16 13:00:41 +0530

    completed init method

--- src/main/java/com/learning/restaurant/impl/FileSystemStorageService.java
+++ src/main/java/com/learning/restaurant/impl/FileSystemStorageService.java
@@ -1,4 +1,51 @@
 package com.learning.restaurant.impl;
 
-public class FileSystemStorageService {
+import com.learning.restaurant.exceptions.StorageException;
+import com.learning.restaurant.services.StorageService;
+import com.nimbusds.jose.util.Resource;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+
+@Service
+@Slf4j
+
+
+public class FileSystemStorageService implements StorageService {
+
+    @Value("${app.storage.location:uploads}")
+    private  String storageLocation;
+
+    private Path rootLocation;
+
+
+    @PostConstruct
+    public  void init(){
+        rootLocation = Paths.get(storageLocation);
+        try {
+            Files.createDirectories(rootLocation);
+        } catch (IOException e) {
+            throw new StorageException("Could not initialize storage location", e);
+        }
+
+    }
+
+    @Override
+    public String store(MultipartFile file, String filename) {
+        return "";
+    }
+
+    @Override
+    public Optional<Resource> loadAsResource(String id) {
+        return Optional.empty();
+    }
 }
