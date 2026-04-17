94e74bd6fb1be0d33e8f3ca05efb308f5122384e
Keerthi <rhkeerthi3@gmail.com>
2026-04-17 10:51:14 +0530

    completed uploadPhoto method

--- src/main/java/com/learning/restaurant/impl/PhotoServiceImpl.java
+++ src/main/java/com/learning/restaurant/impl/PhotoServiceImpl.java
@@ -1,4 +1,37 @@
 package com.learning.restaurant.impl;
 
-public class PhotoServiceImpl {
+
+import com.learning.restaurant.domain.entities.Photo;
+import com.learning.restaurant.services.PhotoService;
+import com.learning.restaurant.services.StorageService;
+import com.nimbusds.jose.util.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PhotoServiceImpl implements PhotoService {
+
+    private  final StorageService storageService;
+    @Override
+
+    public Photo uploadPhoto(MultipartFile file) {
+      String photoId =  UUID.randomUUID().toString();
+        String url = storageService.store(file,photoId);
+
+        return  Photo.builder()
+                .url(url)
+                .uploadDate(LocalDateTime.now())
+                .build();
+    }
+
+    @Override
+    public Optional<Resource> getPhotoAsResource(String id) {
+        return Optional.empty();
+    }
 }
