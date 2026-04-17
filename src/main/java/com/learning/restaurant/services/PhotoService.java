3ae9568fbeee2cdc7763de2257097256363c99f3
Keerthi <rhkeerthi3@gmail.com>
2026-04-17 10:44:46 +0530

    completed PhotoService

--- src/main/java/com/learning/restaurant/services/PhotoService.java
+++ src/main/java/com/learning/restaurant/services/PhotoService.java
@@ -1,4 +1,17 @@
 package com.learning.restaurant.services;
 
+
+//upload and retreive photo
+
+import com.learning.restaurant.domain.entities.Photo;
+import com.nimbusds.jose.util.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+
 public interface PhotoService {
+    Photo uploadPhoto(MultipartFile file);
+    Optional<Resource> getPhotoAsResource(String id);
+
 }
